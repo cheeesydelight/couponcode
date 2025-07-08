08c44b35123de7ed12c89c12bad03df378190525
cheeesydelight <cheeesydelight0@gmail.com>
2025-07-08 13:22:25 +0530

    Add files via upload

--- firebase_util.py
+++ firebase_util.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Path to Firebase service account JSON
-FIREBASE_CRED_PATH = os.getenv("FIREBASE_CRED_JSON", "cheesydelight-service-account.json")
+FIREBASE_CRED_PATH = os.getenv("FIREBASE_CRED_JSON", "./cheesydelight-80a43-firebase-adminsdk-fbsvc-2e5458d166.json")
 FIREBASE_DB_URL = os.getenv("FIREBASE_DB_URL", "https://cheesydelight-80a43-default-rtdb.firebaseio.com")
 
 # Initialize Firebase app if not already initialized
